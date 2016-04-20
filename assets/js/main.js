6c8dd440010ef13b3cc5518d292d3f31e08f020a
_AL_ <al50000@yandex.ru>
2016-04-20 16:55:46 +0300

    header prototype

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
-  $('.btn').slideUp(500).delay(500).slideDown(500);
+  // $(".btn").slideUp(500).delay(500).slideDown(500);
+
 });
 
-console.log('main.js loaded!');
+console.log("main.js loaded!");
